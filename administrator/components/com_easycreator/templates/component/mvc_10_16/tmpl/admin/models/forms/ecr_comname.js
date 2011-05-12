7ee9bfd65fd6ee352db306b5af487f9e5bf3fa80
elkuku <der.el.kuku@gmail.com>
2011-05-12 19:23:55 +0000

    cleanup
    git-svn-id: http://joomlacode.org/svn/elkuku/easy_creator/trunk@393 21ea0da2-e27b-42bb-affe-a196c22f1bbc

--- administrator/components/com_easycreator/templates/component/mvc_10_16/tmpl/admin/models/forms/ecr_comname.js
+++
@@ -1,8 +0,0 @@
-window.addEvent('domready', function()
-{
-    document.formvalidator.setHandler('greeting', function(value)
-    {
-        regex = /^[^0-9]+$/;
-        return regex.test(value);
-    });
-});
--- administrator/components/com_easycreator/templates/component/mvc_10_16/tmpl/admin/views/ecr_comname_ecr_list_postfix/tmpl/default_footer.php
+++
@@ -1,7 +0,0 @@
-<?php
-##*HEADER*##
-
-?>
-<tr>
-	<td colspan="3"><?php echo $this->pagination->getListFooter(); ?></td>
-</tr>
