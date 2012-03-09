36683259f80264dcc8498783c2109d4c5b0d177e
Nikolai Plath <der.el.kuku@gmail.com>
2012-03-09 00:18:34 -0500

    Modify the credits file to include new translators from opentranslators/transifex

--- administrator/components/com_easycreator/views/help/tmpl/default.php
+++ administrator/components/com_easycreator/views/help/tmpl/default.php
@@ -1,4 +1,4 @@
-<?php
+<?php defined('_JEXEC') || die('=;)');
 /**
  * @package    EasyCreator
  * @subpackage Help
@@ -7,63 +7,91 @@
  * @license    GNU/GPL, see JROOT/LICENSE.php
  */
 
-//-- No direct access
-defined('_JEXEC') || die('=;)');
-
 ?>
-<br />
+
 <div class="white_box">
 
-<div style="float: right" class="img128 icon-128-easycreator"></div>
-<h1><?php echo jgettext('Help'); ?></h1>
-<p>
-    <span class="img32a icon-32-help">
-    	EasyCreator documentation can be found on the unofficial german Joomla! wiki Joomla-Nafu.de. Don't worry - it has been written in english language (I hope that you will understand my poor english)
-    	<br />
-    	As this is a wiki (just like wikipedia) - your are hereby invited to contribute or correct my errors - big thanks !
-    	<br />
-    	<br />
-    	So... here it is: <a class="external" href="<?php echo ECR_DOCU_LINK; ?>">EasyCreator Documentation</a> (WIP)
-    </span>
-</p>
+    <div style="float: right" class="img128 icon-128-easycreator"></div>
 
-<h1>Credits</h1>
-<ul>
-    <li>
-        <strong>The icon</strong> belongs to the very funny constructor game <a href="http://games.kde.org/game.php?game=ktuberling" class="external"><span class="img icon-16-easycreator">KTuberling</span></a>. I would call EasyCreator also a "potato editor" <tt>=;)</tt>
-    </li>
-    <li>
-        <strong>EditArea</strong> The great code editor by Christophe Dolivet. <a href="http://www.cdolivet.com/index.php?page=editArea" class="external">www.cdolivet.net/editarea</a>
-    </li>
-    <li>
-        <strong>PHP File Tree</strong> The beautiful File tree is build on the idea by Cory S.N. LaViska - <a href="http://abeautifulsite.net/2007/06/php-file-tree/" class="external">abeautifulsite.net</a> - <em>heavily modified</em>
-    </li>
-    <li>
-        <strong>Manifest maker class</strong> by Ian MacLennan <a href="http://joomlacode.org/gf/project/ianstools/" class="external">joomlacode.org/gf/project/ianstools</a> - <em>slightly modified</em>
-    </li>
-    <li>
-        <strong>Package creation</strong> is done with <a href="http://pear.php.net" class="external">PEAR</a> packages <a href="http://pear.php.net/package/Archive_Tar" class="external">Archive_Tar</a> and <a href="http://pear.php.net/package/Archive_Zip" class="external">Archive_Zip</a>.
-    </li>
-    <li>
-        <strong>Package install routine</strong> for J! 1.5 by Andrew Eddie - New Life in IT Pty Ltd <a href="http://jxtended.com/" class="external">jxtended.com</a>
-    </li>
-    <li>
-        <strong>Diff engine</strong> comes from <a href="http://www.dokuwiki.org" class="external">Dokuwiki</a> - see &copy; + License in helpers/DifferenceEngine.php
-    </li>
-    <li>
-        <strong>Extension templates</strong> see individual templates and most of all <a href="http://docs.joomla.org/" class="external">docs.joomla.org</a>
-    </li>
-    <li>
-        <strong>Fellow translators ::</strong>
-        <ul>
-            <li><strong>French</strong> crony</li>
-            <li><strong>Polish</strong> keran</li>
-            <li><strong>Chinese</strong> baijianpeng <a href="http://www.joomlagate.com" class="external">www.joomlagate.com</a></li>
-        </ul>
-    </li>
-    <li>Who's missing?... ah, me - Nikolai Plath (aka elkuku) - <a href="https://github.com/elkuku" class="external">github.com/elkuku</a> - responsible for the <strong>rest</strong> <tt style="color: green;">=;)</tt></li>
-</ul>
+    <h1><?php echo jgettext('Help'); ?></h1>
+
+    <p>
+    <span class="img32a icon-32-help">
+        EasyCreator documentation can be found on the unofficial german Joomla! wiki Joomla-Nafu.de. in english only.
+        <br/>
+        As this is a wiki (just like wikipedia) - your are hereby invited to contribute or correct any errors - <big>BIG</big> thanks !
+        <br/>
+        <br/>
+        So... here it is: <a class="external" href="<?php echo ECR_DOCU_LINK; ?>">EasyCreator Documentation</a> (WIP)
+    </span>
+    </p>
+
+    <h1>EasyCreator Credits</h1>
+    <ul>
+        <li>
+            <strong>The icon</strong> belongs to the very funny constructor game <a
+            href="http://games.kde.org/game.php?game=ktuberling" class="external"><span class="img icon-16-easycreator">KTuberling</span></a>.
+            I would call EasyCreator also a "potato editor" <tt>=;)</tt>
+        </li>
+        <li>
+            <strong>EditArea</strong> The great code editor by Christophe Dolivet. <a
+            href="http://www.cdolivet.com/index.php?page=editArea" class="external">www.cdolivet.net/editarea</a>
+        </li>
+        <li>
+            <strong>PHP File Tree</strong> The beautiful File tree is build on an idea by Cory S.N. LaViska - <a
+            href="http://abeautifulsite.net/2007/06/php-file-tree/" class="external">abeautifulsite.net</a> - <em>heavily
+            modified</em>
+        </li>
+        <li>
+            <strong>Manifest maker class</strong> by Ian MacLennan <a href="http://joomlacode.org/gf/project/ianstools/"
+                                                                      class="external">joomlacode.org/gf/project/ianstools</a>
+            - <em>slightly modified</em>
+        </li>
+        <li>
+            <strong>Package creation</strong> is done with <a href="http://pear.php.net" class="external">PEAR</a>
+            packages <a href="http://pear.php.net/package/Archive_Tar" class="external">Archive_Tar</a> and <a
+            href="http://pear.php.net/package/Archive_Zip" class="external">Archive_Zip</a>.
+        </li>
+        <li>
+            <strong>Package install routine</strong> for J! 1.5 by Andrew Eddie - New Life in IT Pty Ltd <a
+            href="http://jxtended.com/" class="external">jxtended.com</a>
+        </li>
+        <li>
+            <strong>Diff engine</strong> comes from <a href="http://www.dokuwiki.org" class="external">Dokuwiki</a> -
+            see &copy; + License in helpers/DifferenceEngine.php
+        </li>
+        <li>
+            <strong>Extension templates</strong> see individual templates and most of all <a
+            href="http://docs.joomla.org/" class="external">docs.joomla.org</a>
+        </li>
+        <li>
+            <strong>Fellow translators:</strong>
+            <ul>
+                <li><strong>ar-AR</strong><br/>&lt;ahmadalfy@gmail.com&gt;, 2011.<br/>&lt;m-amine-1995@hotmail.com&gt;,
+                    2012.
+                </li>
+                <li><strong>da-DK</strong><br/>Brian Rønnow &lt;info@b-ronnow.dk&gt;, 2011.</li>
+                <li><strong>de-DE</strong><br/>Michael &lt;michael.pohlhaus@googlemail.com&gt;, 2011.<br/>&lt;bvp@gmx.de&gt;,
+                    2011.
+                </li>
+                <li><strong>es-ES</strong><br/>javier Fernández Llata &lt;jfllata@gmail.com&gt;, 2011.</li>
+                <li><strong>fr-FR</strong><br/>crony, 2008<br/>Marcos Peebles &lt;marcos.peebles@gmail.com&gt;, 2011.
+                </li>
+                <li><strong>hu-HU</strong><br/>Sándor Balikó &lt;vo101sanyo@gmail.com&gt;, 2011.</li>
+                <li><strong>id-ID</strong><br/>&lt;uringinteristi@gmail.com&gt;, 2011.</li>
+                <li><strong>nl-NL</strong><br/>Steven Zeegers &lt;steven.zeegers@gmail.com&gt;, 2011.</li>
+                <li><strong>pl-PL</strong><br/>keran, 2008<br/>Begina Felicysym &lt;begina.felicysym@wp.eu&gt;, 2012.
+                </li>
+                <li><strong>pt-BR</strong><br/>Manoel iikozen &lt;iikozen@yahoo.com&gt;, 2011.</li>
+                <li><strong>sv-SE</strong><br/>Joel Hansen &lt;johaswe@gmail.com&gt;, 2011.<br/>&lt;linus.pettersson@gmail.com&gt;,
+                    2011.<br/>Tess Neale &lt;tess@prothemer.com&gt;, 2011.
+                </li>
+                <li><strong>zh-CN</strong><br/>baijianpeng, 2008</li>
+            </ul>
+        </li>
+        <li>Who's missing ? ... ah, me - Nikolai Plath (aka elkuku) - <a href="https://github.com/elkuku"
+                                                                         class="external">github.com/elkuku</a> -
+            responsible for the <strong>rest</strong> <tt style="color: green;">=;)</tt></li>
+    </ul>
 
 </div>
-<br />
-<?php
