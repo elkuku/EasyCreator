0ab517524a07457a5608887a5070b78757794977
Nikolai Plath <der.el.kuku@gmail.com>
2012-03-29 08:48:43 -0500

    Fix edit link not working from list layout in extension template component mvc_9_16

--- administrator/components/com_easycreator/templates/component/mvc_9_16/tmpl/admin/views/ecr_comname_ecr_list_postfix/tmpl/default_body.php
+++ administrator/components/com_easycreator/templates/component/mvc_9_16/tmpl/admin/views/ecr_comname_ecr_list_postfix/tmpl/default_body.php
@@ -3,17 +3,17 @@
 
 ?>
 <?php foreach($this->items as $i => $row): ?>
-    <?php $link = JRoute::_('index.php?option=_ECR_COM_COM_NAME_&controller=_ECR_COM_TBL_NAME_&task=edit&cid[]='
-    .$row->id); ?>
-    <tr class="row<?php echo $i % 2; ?>">
-		<td>
-			<a href="<?php echo $link; ?>">
-				<?php echo $row->id; ?>
-			</a>
-		</td>
-		<td>
-            <?php echo JHtml::_('grid.id', $i, $row->id); ?>
-        </td>
+<?php $link = JRoute::_('index.php?option=_ECR_COM_COM_NAME_&view=_ECR_COM_TBL_NAME_&layout=edit&id='
+        .$row->id); ?>
+<tr class="row<?php echo $i % 2; ?>">
+    <td>
+        <a href="<?php echo $link; ?>">
+            <?php echo $row->id; ?>
+        </a>
+    </td>
+    <td>
+        <?php echo JHtml::_('grid.id', $i, $row->id); ?>
+    </td>
 <!--admin.viewlist.table._ECR_COM_TBL_NAME_.cell-->
-	</tr>
+</tr>
 <?php endforeach;
