e1ed3ce81d61aa25cdacb6b1d6876779bb5094c3
Nikolai Plath <der.el.kuku@gmail.com>
2012-05-13 23:43:43 -0500

    Formatting

---
+++ administrator/components/com_easycreator/views/deploy/tmpl/package_updatefiles.php
@@ -0,0 +1,21 @@
+<?php defined('_JEXEC') || die('=;)');
+/**
+ * @package    EasyCreator
+ * @subpackage Views
+ * @author     Nikolai Plath (elkuku)
+ * @author     Created on 25-Apr-2011
+ * @license    GNU/GPL, see JROOT/LICENSE.php
+ */
+
+?>
+<div class="infoHeader img icon-24-github">
+    <?php echo jgettext('Update files') ?>
+</div>
+
+<div id="updateFiles"></div>
+
+<div class="buttons">
+    <a class="ecr_button" href="javascript:;"
+       onclick="EcrDeploy.addUpdateFile();"><?php echo jgettext('Add file'); ?></a>
+    <a class="ecr_button" href="javascript:;" onclick="EcrDeploy.saveUpdateFiles();"><?php echo jgettext('Save'); ?></a>
+</div>
