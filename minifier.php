5c5de65c688d39acbb5682e62e1ca8125a724dec
Nikolai Plath <der.el.kuku@gmail.com>
2012-05-15 16:13:33 -0500

    Add a minifier PHP wrapper

---
+++ minifier.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @package    EasyCreator
+ * @subpackage Helpers.others
+ * @author     Nikolai Plath (elkuku)
+ * @author     Created on 15-May-2012
+ * @license    GNU/GPL, see JROOT/LICENSE.php
+ */
+
+$yuiJar = '/home/elkuku/libs/yui/yuicompressor-2.4.7.jar';
+
+$types = array('css', 'js');
+
+$cnt = 0;
+
+$NL = "\n";
+echo 'EasyCreator CSS and JavaScript minifier'.$NL;
+
+foreach($types as $type)
+{
+    echo 'minifying '.$type.' ...';
+
+    $cntA = 0;
+
+    /* @var DirectoryIterator $fileInfo */
+    foreach(new DirectoryIterator(__DIR__.'/administrator/components/com_easycreator/assets/'.$type) as $fileInfo)
+    {
+        if($fileInfo->isDir())
+            continue;
+
+        $parts = (explode('.', $fileInfo->getFilename()));
+
+        if(2 != count($parts))
+            continue;
+
+        $path = $fileInfo->getRealPath();
+
+        $outFile = $fileInfo->getPath().'/'.$parts[0].'.min.'.$parts[1];
+
+        $cmd = 'java -jar '.$yuiJar.' -o '.$outFile.' '.$path;
+
+        passthru($cmd);
+
+        $cntA ++;
+    }
+    echo sprintf('%d files have been minified', $cntA).$NL;
+
+    $cnt += $cntA;
+}
+
+echo $NL.sprintf('Finished. Total: %d files', $cnt).$NL;
