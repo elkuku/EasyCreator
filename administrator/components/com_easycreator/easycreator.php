6beafdec2eab2010706541f2084e05fc65c48f55
Nikolai Plath <der.el.kuku@gmail.com>
2012-09-26 09:14:17 -0500

    Restructuration

--- administrator/components/com_easycreator/easycreator.php
+++ administrator/components/com_easycreator/easycreator.php
@@ -21,78 +21,16 @@ JDEBUG ? JProfiler::getInstance('Application')->mark('com_easycreator starting')
 jimport('joomla.filesystem.folder');
 jimport('joomla.filesystem.file');
 
+JHtml::_('behavior.framework');
+JHTML::_('behavior.tooltip');
+
 //-- Global constants
 require JPATH_COMPONENT.'/includes/defines.php';
 
-//-- Global functions
-require JPATH_COMPONENT.'/includes/loader.php';
-
-if(ECR_DEV_MODE)
-{
-    //-- Setup debugger
-    if(JComponentHelper::getParams('com_easycreator')->get('ecr_debug'))
-    {
-        //-- Set debugging ON
-        define('ECR_DEBUG', true);
-    }
-    else
-    {
-        define('ECR_DEBUG', false);
-    }
-
-    if(JComponentHelper::getParams('com_easycreator')->get('ecr_debug_lang', 0))
-    {
-        //-- Set debugging ON
-        define('ECR_DEBUG_LANG', 1);
-    }
-    else
-    {
-        define('ECR_DEBUG_LANG', 0);
-    }
-}
-else
-{
-    define('ECR_DEBUG', 0);
-}
-
-//-- Load the special Language
-
-//-- 1) Check if g11n is installed as a PEAR package - see: http://elkuku.github.com/pear/
-
-//@include 'elkuku/g11n/language.php';
-
-//-- @todo: check for installed g11n PEAR package to remove the "shut-up"
-//-- @require_once 'elkuku/g11n/language.php';
-
 try
 {
-    if( ! class_exists('g11n'))
-    {
-        //-- 2) Check the libraries folder
-
-        if( ! jimport('g11n.language'))
-        {
-            //-- 3) Load a dummy language handler -> english only !
-
-            ecrLoadHelper('g11n_dummy');
-
-            ecrScript('g11n_dummy', 'php2js');
-        }
-    }
-
-    if(class_exists('g11n'))
-    {
-        //-- TEMP@@debug
-        if(ECR_DEV_MODE && ECR_DEBUG_LANG)
-        {
-            //-- @@DEBUG
-            g11n::cleanStorage();
-            g11n::setDebug(ECR_DEBUG_LANG);
-        }
-
-        //-- Get our special language file
-        g11n::loadLanguage();
-    }
+    //-- Global functions
+    require JPATH_COMPONENT.'/includes/loader.php';
 }
 catch(Exception $e)
 {
@@ -114,23 +52,34 @@ define('ECR_VERSION', EcrProjectHelper::parseXMLInstallFile(
  */
 switch(ECR_JVERSION)
 {
+    case '1.5' :
+    case '1.6' :
+    case '1.7' :
+    JFactory::getApplication()->enqueueMessage(sprintf(
+            jgettext('EasyCreator %1$s is not compatible with Joomla! %2$s - Sorry.')
+            , ECR_VERSION, ECR_JVERSION)
+        , 'error');
+
+    return;
+        break;
+
     case '3.0': //-- Get prepared
         $application = JFactory::getApplication();
 
         $application->JComponentTitle = 'EasyCreator';
 
-        /*
         $application->enqueueMessage(sprintf(
             jgettext(
-                'EasyCreator version %s is in testing stage with your Joomla! version %s'
+                'EasyCreator %1$s is in testing stage with Joomla! %2$s'
             )
             , ECR_VERSION, ECR_JVERSION), 'warning'
         );
-        */
+
         break;
 
     case '2.5':
-        //-- We're all OK
+        //-- @Joomla!-compat 2.5
+        ecrStylesheet('bootstrap');
         break;
 
     default:
@@ -141,19 +90,13 @@ switch(ECR_JVERSION)
 }
 
 //-- Add CSS
-ecrStylesheet('bootstrap', 'default', 'toolbar', 'icon');
-
-//-- @todo remove bootstrap when it is integrated in J! 3.0
-#ecrStylesheet('default', 'toolbar', 'icon');
-
-//-- Setup tooltips - used almost everywhere..
-JHTML::_('behavior.tooltip');
+ecrStylesheet('default', 'toolbar', 'icon');
 
 //-- Add JavaScript
 ecrScript('global_vars', 'easycreator');
 
-JFactory::getDocument()->addScriptDeclaration("var ECR_JVERSION = '".ECR_JVERSION."';".NL);
 JFactory::getDocument()->addScriptDeclaration("var ECR_VERSION = '".ECR_VERSION."';".NL);
+JFactory::getDocument()->addScriptDeclaration("var ECR_JVERSION = '".ECR_JVERSION."';".NL);
 
 $prevErrorReporting = error_reporting(- 1);
 
--- administrator/components/com_easycreator/includes/loader.php
+++ administrator/components/com_easycreator/includes/loader.php
@@ -9,6 +9,108 @@
  * @license    GNU/GPL, see JROOT/LICENSE.php
  */
 
+if(version_compare(PHP_VERSION, '5.3', '<'))
+{
+    //-- @todo php 5.2
+    spl_autoload_register('easy_creator_loader', true);
+}
+else
+{
+    spl_autoload_register('easy_creator_loader', true, true);
+}
+
+//-- Load the special Language
+
+//-- 1) Check if g11n is installed as a PEAR package - see: http://elkuku.github.com/pear/
+
+//@include 'elkuku/g11n/language.php';
+
+//-- @todo: check for installed g11n PEAR package to remove the "shut-up"
+//-- @require_once 'elkuku/g11n/language.php';
+
+if( ! class_exists('g11n'))
+{
+    //-- 2) Check the libraries folder
+
+    if( ! jimport('g11n.language'))
+    {
+        //-- 3) Load a dummy language handler -> english only !
+
+        ecrLoadHelper('g11n_dummy');
+
+        ecrScript('g11n_dummy', 'php2js');
+    }
+}
+
+if(class_exists('g11n'))
+{
+    //-- TEMP@@debug
+    if(ECR_DEV_MODE && ECR_DEBUG_LANG)
+    {
+        //-- @@DEBUG
+        g11n::cleanStorage();
+        g11n::setDebug(ECR_DEBUG_LANG);
+    }
+
+    //-- Get our special language file
+    g11n::loadLanguage();
+}
+
+/*
+ * Functions
+ */
+
+/**
+ * Autoloader.
+ *
+ * @param $className
+ *
+ * @return mixed
+ */
+function easy_creator_loader($className)
+{
+    if(0 !== strpos($className, 'Ecr'))
+        return;
+
+    $base = JPATH_COMPONENT_ADMINISTRATOR.'/helpers';
+
+    $file = strtolower(substr($className, 3));
+
+    $path = $base.'/'.$file.'.php';
+
+    //-- 1. search in 'helpers/<file>.php'
+    if(file_exists($path))
+    {
+        include $path;
+
+        return;
+    }
+
+    //-- 2. search in 'helpers/<path>/<file>.php'
+    $parts = preg_split('/(?<=[a-z])(?=[A-Z])/x', substr($className, 3));
+
+    $path = $base.'/'.strtolower(implode('/', $parts)).'.php';
+
+    if(file_exists($path))
+    {
+        include $path;
+
+        return;
+    }
+
+    //-- 3. search in 'helpers/<path>/<file>/<file>.php'
+    $file = strtolower($parts[count($parts) - 1]);
+
+    $path = $base.'/'.strtolower(implode('/', $parts)).'/'.$file.'.php';
+
+    if(file_exists($path))
+    {
+        include $path;
+
+        return;
+    }
+}
+
 /**
  * Loads EasyCreator Helpers.
  *
@@ -93,66 +195,5 @@ function ecrLoadMedia()
 
         $document->addStylesheet(JUri::root(true).'/media/com_easycreator/admin/css/'.$name.'.css');
         $document->addScript(JURI::root(true).'/media/com_easycreator/admin/js/'.$name.'.js');
-    }
-}
-
-if(version_compare(PHP_VERSION, '5.3', '<'))
-{
-    //-- @todo php 5.2
-    spl_autoload_register('easy_creator_loader', true);
-}
-else
-{
-    spl_autoload_register('easy_creator_loader', true, true);
-}
-
-/**
- * Autoloader.
- *
- * @param $className
- *
- * @return mixed
- */
-function easy_creator_loader($className)
-{
-    if(0 !== strpos($className, 'Ecr'))
-        return;
-
-    $base = JPATH_COMPONENT_ADMINISTRATOR.'/helpers';
-
-    $file = strtolower(substr($className, 3));
-
-    $path = $base.'/'.$file.'.php';
-
-    //-- 1. search in 'helpers/<file>.php'
-    if(file_exists($path))
-    {
-        include $path;
-
-        return;
-    }
-
-    //-- 2. search in 'helpers/<path>/<file>.php'
-    $parts = preg_split('/(?<=[a-z])(?=[A-Z])/x', substr($className, 3));
-
-    $path = $base.'/'.strtolower(implode('/', $parts)).'.php';
-
-    if(file_exists($path))
-    {
-        include $path;
-
-        return;
-    }
-
-    //-- 3. search in 'helpers/<path>/<file>/<file>.php'
-    $file = strtolower($parts[count($parts) - 1]);
-
-    $path = $base.'/'.strtolower(implode('/', $parts)).'/'.$file.'.php';
-
-    if(file_exists($path))
-    {
-        include $path;
-
-        return;
     }
 }
