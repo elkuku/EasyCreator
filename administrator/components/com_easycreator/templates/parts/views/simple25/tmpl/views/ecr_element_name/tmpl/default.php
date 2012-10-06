01292d2dff4161409cd66685e8ef644eab3b0121
Nikolai Plath <der.el.kuku@gmail.com>
2012-10-06 11:34:29 -0500

    Add simple view template for J 2.5

---
+++ administrator/components/com_easycreator/templates/parts/views/simple25/tmpl/views/ecr_element_name/tmpl/default.php
@@ -0,0 +1,4 @@
+<?php
+##*HEADER*##
+
+echo 'default template for ECR_ELEMENT_NAME view';
---
+++ administrator/components/com_easycreator/templates/parts/views/simple25/tmpl/views/ecr_element_name/view.html.php
@@ -0,0 +1,27 @@
+<?php
+##*HEADER*##
+
+jimport('joomla.application.component.view');
+
+/**
+ * ECR_ELEMENT_NAME view.
+ *
+ * @package    ECR_COM_NAME
+ * @subpackage Views
+ */
+class ECR_COM_NAMEViewECR_ELEMENT_NAME extends JView
+{
+    /**
+     * ECR_COM_NAMEECR_LIST_POSTFIX view display method.
+     *
+     * @param string $tpl The name of the template file to parse;
+     *
+     * @return void
+     */
+    public function display($tpl = null)
+    {
+        $foo = 'Do something here..';
+
+        parent::display($tpl);
+    }//function
+}//class
