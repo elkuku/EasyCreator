59f4d889bc8bfd978fc9e981e8364a08b9f0be59
Nikolai Plath <der.el.kuku@gmail.com>
2015-01-10 09:41:48 -0500

    Fix a doc block

--- admin/helpers/g11n_dummy.php
+++ admin/helpers/g11n_dummy.php
@@ -11,32 +11,27 @@
 defined('_JEXEC') || die('=;)');
 
 /**
- * PHP dummy function
- */
-
-/**
  * PHP dummy function.
  *
- * @param string $string The string
+ * @param string $string The string.
  *
  * @return string
  */
 function jgettext($string)
 {
     return $string;
-}//function
+}
 
 /**
  * PHP dummy function.
  *
- * @param string  $singular Singular string
- * @param string  $plural
- * @param integer $count    Count
+ * @param string  $singular Singular string.
+ * @param string  $plural   Plural string.
+ * @param integer $count    Count.
  *
- * @internal param string $pluralPlural string
  * @return string
  */
 function jngettext($singular, $plural, $count)
 {
     return (1 == $count) ? $singular : $plural;
-}//function
+}
