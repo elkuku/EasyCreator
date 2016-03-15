4a70c2c2cfd3a9359a3577c985e19ec32e7de44b
Douglas <machadoug@users.noreply.github.com>
2016-03-15 15:03:21 -0300

    Add compatibility with Joomla 3.5

--- admin/helpers/project/type/plugin.php
+++ admin/helpers/project/type/plugin.php
@@ -168,6 +168,7 @@ class EcrProjectTypePlugin extends EcrProjectBase
             case '3.2':
 	        case '3.3':
 	        case '3.4':
+	        case '3.5':
 	        break;
 
             default:
