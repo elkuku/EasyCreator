25584cccb295521cf642df0eb87a7944c872af6d
Nikolai Plath <der.el.kuku@gmail.com>
2017-09-27 10:03:42 -0500

    Remove obsolete links

--- admin/views/easycreator/tmpl/default.php
+++ admin/views/easycreator/tmpl/default.php
@@ -7,8 +7,13 @@
  * @license    GNU/GPL, see JROOT/LICENSE.php
  */
 
-//-- No direct access
 defined('_JEXEC') || die('=;)');
+
+$link = new stdClass;
+
+$link->repo = 'https://github.com/elkuku/EasyCreator/';
+$link->bugTracker = 'https://github.com/elkuku/EasyCreator/issues';
+$link->latestVersion = 'https://github.com/elkuku/EasyCreator/commits/master.atom';
 
 if( ! JComponentHelper::getParams('com_easycreator')->get('cred_author')) :
     //-- Parameters have not been set
@@ -16,35 +21,34 @@ if( ! JComponentHelper::getParams('com_easycreator')->get('cred_author')) :
     JFactory::getApplication()->enqueueMessage(
         sprintf(jgettext('Please set your personal information in %s'), $link), 'warning');
 endif;
-//<a class="ecr_modal" href="http://joomla.org">@TEST</a>
-//<div onclick="checkVersion();">Check</div>
 ?>
 <div class="ecrInfoBoxContainer">
-    <div class="btn block" onclick="ecrInfoBox.toggle();">
+    <div class="btn block">
         <i class="img icon16-easycreator"></i>
         <?php echo jgettext('EasyCreator Information'); ?>
     </div>
-    <div id="ecrInfoBox" style="background-color: #ccc;">
-        <?php echo $this->loadTemplate('ecrbox'); ?>
+    <div>
+        <ul style="list-style: none; margin: 0; padding: 0;">
+            <li class="img icon16-git">
+                <a href="<?php echo $link->repo; ?>" class="external">
+                    <?php echo jgettext('Git repository'); ?>
+                </a>
+            </li>
+            <li class="img icon16-bug">
+                <a href="<?php echo $link->bugTracker; ?>"
+                   class="external">
+                    <?php echo jgettext('Bugtracker'); ?>
+                </a>
+            </li>
+            <li class="img icon16-rss">
+                <a href="<?php echo $link->latestVersion; ?>" class="external">
+                    <?php echo jgettext('Recent commits'); ?>
+                </a>
+            </li>
+        </ul>
     </div>
 </div>
 
-<?php
-/*
-if(JComponentHelper::getParams('com_easycreator')->get('versionCheck')) :
-    if(JFactory::getSession()->get('ecr_versionCheck')) :
-        //-- Do smthng ?
-    //else :
-        echo '<div id="ecr_versionCheck">';
-        JFactory::getDocument()->addScriptDeclaration("window.addEvent('domready', function() { checkVersion(); });");
-        echo '</div>';
-        JFactory::getSession()->set('ecr_versionCheck', 'checked');
-    endif;
-else:
-    echo jgettext('Version check is disabled');
-endif;
-*/
-?>
 <div style="text-align: center">
     <span class="img128 icon128-easycreator"></span>
     <h1 class="ecrTitel">What do you want to Create today ?</h1>
