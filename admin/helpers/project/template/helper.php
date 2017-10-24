edf8f49e700ded4a08c5b17b1917995b661c7ee1
Nikolai Plath <der.el.kuku@gmail.com>
2017-10-24 11:35:26 -0500

    Set CURLOPT_SSL_VERIFYPEER to false

--- admin/helpers/project/template/helper.php
+++ admin/helpers/project/template/helper.php
@@ -331,7 +331,8 @@ class EcrProjectTemplateHelper
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
         $data = curl_exec ($ch);
